32743fd1de96042573554dea2ec54103ebec13b6
Rohan Khan <rohankhan170@gmail.com>
2019-10-17 17:08:42 +0600

    Added README and changed structure to be a module

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,4 @@
-import * as React from 'react';
-import { render } from 'react-dom';
 import Stepper from './Stepper';
 import Step from './Step';
 
-import './styles.css';
-
-const App: React.FC = () => {
-	return (
-		<div className="App">
-			<Stepper withNumbers activeStep={1}>
-				<Step>Activate powerloop</Step>
-				<Step>Traverse perilous wastes</Step>
-				<Step>Jump the shark</Step>
-			</Stepper>
-		</div>
-	);
-};
-
-const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+export { Stepper, Step };
